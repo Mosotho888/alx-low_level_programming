fc942fcf536368bf16a6a35edb3891f45e5d102c
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-09 11:18:43 -0700

    I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *     main - main block
+ *     Return: 0
+ */
+int main(void)
+{
+	char yes = 'Y';
+
+	putchar(yes);
+	putchar(tolower(yes));
+	return (0);
+}
