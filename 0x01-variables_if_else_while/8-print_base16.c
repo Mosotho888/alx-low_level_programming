f4547f12479f458bc2f65aabb856c8ca6b4c5380
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-09 12:27:00 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *     main - main block
+ *     Return: 0
+ */
+int main(void)
+{
+	char letter;
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar((number % 10) + '0');
+	}
+	for (letter = 'a'; letter >= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
