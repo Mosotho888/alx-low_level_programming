a37ac07c78ea107e19667c2fbb2d61ef0c4bd326
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-09 12:02:41 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *     main - main block
+ *     Return: 0
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 10; numbers++)
+	{
+		printf("%d", numbers);
+	}
+	printf("\n");
+	return (0);
+}
