212c07f2c1182d4a476671e5730bae4f71e296bc
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-09 13:05:13 -0700

    The success combination in business is

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar((number1 % 10) + '0');
 				putchar((number2 % 10) + '0');
 				putchar((number3 % 10) + '0');
-				if (number1 == 7 && number2 == 8 && number == 9)
+				if (number1 == 7 && number2 == 8 && number3 == 9)
 				{
 					continue;
 				}
