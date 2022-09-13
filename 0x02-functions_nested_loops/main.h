6cc4f85bba4d80c1097c2da8b55d9d3c67450df0
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-13 08:43:01 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *     description: Header file containing declarations for all functions
+ *     
+ */
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif
