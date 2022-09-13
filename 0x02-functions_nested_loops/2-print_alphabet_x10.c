37a2d95eaa321ab14d4e7ad6e0c3c9f4519912ad
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-13 08:57:48 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,15 @@
 void print_alphabet_x10(void)
 {
 	char letter;
+	int counter = 0;
 
-	for (int counter = 0; counter <= 9; counter++)
+	while (counter <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
 		_putchar('\n');
+		counter++;
 	}
 }
