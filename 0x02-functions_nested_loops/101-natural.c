6f4d8e8ce5c2c29ee4fe26dc88dc2aad3d0371f3
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-13 13:50:37 -0700

    All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ *     main - computes and prints the sum of all the multiples of 3
+ *            or 5 below 1024, followed by a new line.
+ *     Return: 0
+ */
+int main(void)
+{
+	int number, total = 0;
+
+	for (number = 0; number < 1024; number++)
+	{
+		if (((number % 3) == 0) || ((number % 5) == 5))
+		{
+			total += number;
+		}
+	}
+	printf("%d\n", total);
+}
