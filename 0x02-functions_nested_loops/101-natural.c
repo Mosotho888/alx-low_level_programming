461e8c3440d1a420226419d031687349136bdce4
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-13 14:00:15 -0700

    All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,7 +14,7 @@ int main(void)
 
 	for (number = 0; number < 1024; number++)
 	{
-		if (((number % 3) == 0) || ((number % 5) == 5))
+		if (((number % 3) == 0) || ((number % 5) == 0))
 		{
 			total += number;
 		}
