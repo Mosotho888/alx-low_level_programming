6f81c14be979ed881c0bd7f8e372b22525c6135e
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-19 10:35:09 -0700

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *    rev_string - Reverse the string.
+ *    @s: The string to be reversed.
+ */
+void rev_string(char *s)
+{
+	int length = 0, index = 0;
+	char tmp;
+
+	length = _strlen(s);
+	for (index = length - 1; index >= (length / 2); index--)
+	{
+		tmp = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = tmp;
+	}
+}
