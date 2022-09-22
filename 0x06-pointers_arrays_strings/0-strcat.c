4aa23e354093c7a21f5e43205851235135aadd6f
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-22 12:17:06 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,18 +6,18 @@
  *   @src: The string to be appended to dest.
  *   Return: A pointer to the destination string dest.
  */
-char *strcat(char *dest, const char *src)
+char *strcat(char *dest, char *src)
 {
-	int index = 0, dest_length = 0;
+	int index = 0, dest_length = 0, counter;
 
 	while (dest[index++])
 	{
 		dest_length++;
 	}
-	for (index = 0; src[index]; index++)
+	for (counter = 0; src[counter]; counter++)
 	{
-		dest[dest_length++] = src[index];
+		dest[dest_length++] = src[counter];
 	}
-	dest[dest_length + index] = '\0';
+	dest[dest_length + counter] = '\0';
 	return (dest);
 }
