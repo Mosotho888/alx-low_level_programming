9017045cc057b810886bb454e8dfc71260d7e090
Teboho <tebohogivenmofokeng@gmail.com>
2022-09-30 13:42:49 -0700

    correction

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,12 @@
 /**
  *   _abs - Computes the absolute value of an integer.
  *   @n: The integer to be computed.
- *    
  *   Return: The absolute value of the integer.
  */
 int _abs(int n)
 {
 	if (n >= 0)
-	{
+	i{
 		return (n);
 	}
 	else
